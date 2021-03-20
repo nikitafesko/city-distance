be7eab477d0a7215950c05ceb7dcb3ba859008ba
Nikita <58188102+NickDesire@users.noreply.github.com>
2021-03-20 10:02:10 +0300

    Add files via upload

---
+++ CityDistance/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace CityDistance
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1010, 451);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1173, 181);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(195, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Open picture(bmp)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1045, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 55);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "City Distance";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1028, 409);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 54);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Gistogramma";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(1045, 73);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(122, 48);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Change White-Black";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1173, 386);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(122, 30);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Save bmp";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(1045, 127);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(122, 39);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Change to RGB";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // chart1
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            this.chart1.DataSource = this.chart1.Annotations;
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(12, 469);
+            this.chart1.Name = "chart1";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(1384, 302);
+            this.chart1.TabIndex = 9;
+            this.chart1.Text = "chart1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(1173, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(195, 163);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(1173, 217);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(195, 163);
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(1173, 422);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(122, 36);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "Save pol";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1408, 783);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ CityDistance/Form1.cs
@@ -0,0 +1,444 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace CityDistance
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            chart1.ChartAreas[0].AxisX.Interval = 0.1;
+        }
+        string file;
+        Bitmap myBitmap;
+        double[,] matr;
+        int[,] znak;
+        Bitmap secondBitMap;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                openFileDialog1 = new OpenFileDialog
+                {
+                    InitialDirectory = @"C:\",
+                    Title = "Browse Text Files",
+
+                    CheckFileExists = true,
+                    CheckPathExists = true,
+
+                    DefaultExt = "bmp",
+                    Filter = "bmp files (*.bmp)|*.bmp",
+                    FilterIndex = 2,
+                    RestoreDirectory = true,
+
+                    ReadOnlyChecked = true,
+                    ShowReadOnly = true
+                };
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    dataGridView1.Rows.Clear();
+
+                    pictureBox2.Hide();
+                    pictureBox1.Show();
+                    dataGridView1.Show();
+                    file = openFileDialog1.FileName;
+                    myBitmap = new Bitmap(file);
+
+                    pictureBox1.Width = myBitmap.Width + 120;
+                    pictureBox1.Height = myBitmap.Height + 120;
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+
+                    pictureBox1.Image = myBitmap;
+
+                    var bmpWidth = myBitmap.Width;
+                    var bmpHeight = myBitmap.Height;
+                    int[,] pixels = new int[bmpWidth, bmpHeight];
+
+                    dataGridView1.Columns.Clear();
+                    for (int i = 0; i < bmpWidth; i++)
+                    {
+                        dataGridView1.Columns.Add(i + 1 + "", i + 1 + "");
+
+                    }
+
+                    dataGridView1.Rows.Add(bmpWidth - 1);
+
+                    for (int i = 0; i < bmpWidth; i++)
+                    {
+                        DataGridViewRow row2 = dataGridView1.Rows[i];
+                        dataGridView1.Columns[i].Width = 50;
+                    }
+
+                    znak = new int[bmpWidth, bmpHeight];
+                    for (int i = 0; i < bmpWidth; i++)
+                    {
+                        for (int j = 0; j < bmpHeight; j++)
+                        {
+                            Color a = myBitmap.GetPixel(j, i);
+                            if (a.R == 255 && a.G == 255 && a.B == 255)
+                            {
+                                pixels[i, j] = 0;
+                                znak[i, j] = 0;
+                            }
+                            else
+                            {
+                                pixels[i, j] = 1;
+                                znak[i, j] = 1;
+                            }
+                            dataGridView1[i, j].Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                            dataGridView1[j, i].Value = pixels[i, j];
+
+                        }
+
+                    }
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            button2.Enabled = true;
+            button5.Enabled = true;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button3.Enabled = true;
+            button4.Enabled = true;
+
+
+
+
+            for (int i = 0; i < myBitmap.Width; i++)
+            {
+                DataGridViewRow row2 = dataGridView1.Rows[i];
+                dataGridView1.Columns[i].Width = 50;
+            }
+            dataGridView1.ScrollBars = ScrollBars.None;
+
+
+            var res = GetCalculateEwclidDistance(GetBitArray());
+
+            int k = 0;
+
+            for (int i = 0; i < myBitmap.Width; i++)
+            {
+                for (int j = 0; j < myBitmap.Height; j++)
+                {
+                    if (znak[i, j] == 0)
+                    {
+                        dataGridView1[j, i].Value = res[k] * -1;
+
+                    }
+                    else
+                    {
+                        dataGridView1[j, i].Value = res[k];
+                    }
+                    k++;
+                }
+            }
+        }
+        public static Bitmap GrauwertBild(Bitmap input)
+        {
+            Bitmap greyscale = new Bitmap(input.Width, input.Height);
+
+            for (int x = 0; x < input.Width; x++)
+            {
+                for (int y = 0; y < input.Height; y++)
+                {
+                    Color pixelColor = input.GetPixel(x, y);
+                    if (pixelColor.R == 0 && pixelColor.G == 0 && pixelColor.B == 0)
+                    {
+                        greyscale.SetPixel(x, y, Color.FromArgb(pixelColor.A, 255, 255, 255));
+                    }
+                    else
+                    {
+                        greyscale.SetPixel(x, y, Color.FromArgb(pixelColor.A, 0, 0, 0));
+                    }
+                }
+            }
+            return greyscale;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            button6.Enabled = true;
+            pictureBox2.Show();
+            chart1.Series[0].Points.Clear();
+            matr = new double[myBitmap.Width, myBitmap.Height];
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                {
+                    matr[i, j] = double.Parse(dataGridView1.Rows[i].Cells[j].Value.ToString());
+                }
+            }
+
+
+            int Count = 0;     ///Коллво цветов в изобр
+
+            Random random = new Random();
+
+
+            Dictionary<double, double> rezul = new Dictionary<double, double>(); //считаем эл-ты в массиве
+            for (int i = 0; i < matr.GetLength(0); i++)
+            {
+                for (int j = 0; j < matr.GetLength(1); j++)
+                {
+                    if (rezul.ContainsKey(matr[i, j]))
+                    {
+                        rezul[matr[i, j]]++;
+                    }
+                    else
+                    {
+                        rezul.Add(matr[i, j], 1); Count++;
+                    }
+                }
+
+            }
+
+            double[] difColor;
+            double[] countColor;
+            difColor = new double[Count];
+            countColor = new double[Count];
+            int l = 0;
+
+            foreach (KeyValuePair<double, double> par in rezul)
+            {
+
+                chart1.Series[0].Points.AddXY(par.Key, par.Value);
+                chart1.Series[0].IsValueShownAsLabel = true;
+                difColor[l] = par.Key;
+                countColor[l] = par.Value;
+                l++;
+
+            }
+
+            Array.Sort(difColor);
+            Array.Reverse(difColor);
+
+            int indexForColor = 255 / Count;
+            int Position;
+
+
+            secondBitMap = new Bitmap(myBitmap.Width, myBitmap.Height);
+
+            for (int i = 0; i < secondBitMap.Width; i++)
+            {
+                for (int j = 0; j < secondBitMap.Height; j++)
+                {
+
+                    for (int ii = 0; ii < difColor.GetLength(0); ii++)
+                    {
+                        if (matr[i, j] == difColor[ii])
+                        {
+                            Position = ii + 1;
+                            Color newColor = Color.FromArgb(Position * indexForColor, Position * indexForColor, Position * indexForColor);
+                            secondBitMap.SetPixel(j, i, newColor);
+
+                        }
+
+
+                    }
+
+                }
+            }
+
+            pictureBox2.Image = secondBitMap;
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.Width = secondBitMap.Width + 120;
+            pictureBox2.Height = secondBitMap.Height + 120;
+        }
+        private double[,] GetBitArray()
+        {
+            var bmpWidth = myBitmap.Width;
+            var bmpHeight = myBitmap.Height;
+            int[,] pixels = new int[bmpWidth, bmpHeight];
+            double[,] res = new double[bmpWidth, bmpHeight];
+
+            for (int i = 0; i < bmpWidth; i++)
+            {
+                for (int j = 0; j < bmpHeight; j++)
+                {
+                    Color a = myBitmap.GetPixel(j, i);
+                    if (a.R == 255 && a.G == 255 && a.B == 255)
+                    {
+                        pixels[i, j] = 0;
+                    }
+                    else
+                    {
+                        pixels[i, j] = 1;
+                    }
+
+                    res[i, j] = pixels[i, j];
+                }
+            }
+
+            return res;
+        }
+        public List<double> GetCalculateEwclidDistance(double[,] pointss)
+        {
+            Dictionary<string, List<string>> indexes = new Dictionary<string, List<string>>();
+            for (int i = 0; i < myBitmap.Width; i++)
+            {
+                for (int j = 0; j < myBitmap.Height; j++)
+                {
+                    if (pointss[i, j] == 0)
+                    {
+                        indexes.Add(i + "," + j, GetIndex(1, pointss));
+                    }
+                    if (pointss[i, j] == 1)
+                    {
+                        indexes.Add(i + "," + j, GetIndex(0, pointss));
+                    }
+                }
+            }
+
+            List<double> result = new List<double>();
+            foreach (var item in indexes)
+            {
+                var itemArray = item.Key.Split(',').ToArray();
+                var itemValues = item.Value.Select(x => x.Split(',').ToArray()).ToArray();
+                List<double> res = new List<double>();
+
+                for (int i = 0; i < itemValues.Length; i++)
+                {
+                    var a = itemValues[i][0];
+                    var b = itemValues[i][1];
+                    var c = itemArray[0];
+                    var d = itemArray[1];
+
+                    res.Add(Distance(double.Parse(itemArray[0]), double.Parse(itemArray[1]), double.Parse(itemValues[i][0]), double.Parse(itemValues[i][1])));
+                }
+
+                result.Add(res.Min());
+            }
+
+            return result;
+        }
+
+        int globalIForOne = 0;
+        int globalJForOne = 0;
+        int globalIForZero = 0;
+        int globalJForZero = 0;
+
+        private List<string> GetIndex(int number, double[,] pointss)
+        {
+            List<string> result = new List<string>();
+            if (number == 1)
+            {
+                for (int i = 0; i < myBitmap.Width; i++)
+                {
+                    for (int j = 0; j < myBitmap.Height; j++)
+                    {
+                        if (pointss[i, j] == 1 && i >= globalIForOne && j >= globalJForOne)
+                        {
+                            globalIForOne = i;
+                            globalJForOne = j;
+                            result.Add(i + "," + j);
+                        }
+                    }
+                    globalJForOne = 0;
+                }
+            }
+            if (number == 0)
+            {
+                for (int i = 0; i < myBitmap.Width; i++)
+                {
+                    for (int j = 0; j < myBitmap.Height; j++)
+                    {
+                        if (pointss[i, j] == 0 && i >= globalIForZero && j >= globalJForZero)
+                        {
+                            globalIForZero = i;
+                            globalJForZero = j;
+                            result.Add(i + "," + j);
+                        }
+                    }
+                    globalJForZero = 0;
+                }
+            }
+
+
+            globalIForOne = 0;
+            globalJForOne = 0;
+            globalIForZero = 0;
+            globalJForZero = 0;
+            return result;
+        }
+
+
+        public double Distance(double x1, double y1, double x2, double y2) => Math.Round(Math.Abs(x1 - x2) + Math.Abs(y1 - y2));
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var changebleBitmap = new Bitmap(myBitmap.Width, myBitmap.Height);
+            for (int x = 0; x < changebleBitmap.Width; x++)
+            {
+                for (int y = 0; y < changebleBitmap.Height; y++)
+                {
+                    if (int.Parse(dataGridView1[x, y].Value.ToString()) == 1)
+                    {
+                        changebleBitmap.SetPixel(x, y, Color.Black);
+                    }
+                    else
+                    {
+                        changebleBitmap.SetPixel(x, y, Color.White);
+                    }
+                }
+            }
+
+            pictureBox1.Image = changebleBitmap;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Bitmap a2 = new Bitmap(pictureBox2.Image);
+            dataGridView1.ScrollBars = ScrollBars.Horizontal;
+            for (int i = 0; i < a2.Width; i++)
+            {
+                DataGridViewRow row2 = dataGridView1.Rows[i];
+                dataGridView1.Columns[i].Width = 120;
+            }
+            dataGridView1.ScrollBars = ScrollBars.Horizontal;
+            for (int i = 0; i < a2.Width; i++)
+            {
+                for (int j = 0; j < a2.Height; j++)
+                {
+                    Color a = a2.GetPixel(j, i);
+                    dataGridView1[j, i].Value = a.R + "-" + a.G + "-" + a.B;
+
+                }
+
+            }
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image.Save("test.bmp", ImageFormat.Bmp);
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Image.Save("test2.bmp", ImageFormat.Bmp);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
